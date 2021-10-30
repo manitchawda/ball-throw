803a514c5c40aa574a4439713dce22c44b40d3f6
manitchawda <85694963+manitchawda@users.noreply.github.com>
2021-10-30 14:56:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+var engine,world;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1500, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+var ball_options={
+isStatic:false,
+restitution:0.8,
+friction:0,
+density:1.2
+}
+ball=Matter.Bodies.circle(260,100,20,ball_options);
+World.add(world,ball);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ellipse(ball.position.x,ball.position.y,20,20);
+  drawSprites();
+ 
+}
+
+
+
